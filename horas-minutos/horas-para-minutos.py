55153bb16653b59ceb30a0c49c9e0f726a246eb0
Marcos Serra <marcosserra133@gmail.com>
2023-09-06 00:09:16 -0300

    update

--- horas-minutos/horas-para-minutos.py
+++ horas-minutos/horas-para-minutos.py
@@ -5,7 +5,7 @@ horas = int(horas)
 minutos = float(minutos)
 
 minutos_decimal = minutos / 60
-minutos_formatados = "{:.2f}".format(minutos_decimal)
+minutos_formatados = "{:.2f}".format(minutos_decimal) # Formatando os numeros com duas casas decimais
 
 # Verifique se os minutos são menores que 10 e, se sim, remova o zero à esquerda
 if minutos_decimal < 10:
