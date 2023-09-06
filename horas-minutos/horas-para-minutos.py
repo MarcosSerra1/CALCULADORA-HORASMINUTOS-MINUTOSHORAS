cb73b429fa376ef9aa7c2e6539f560f233fcafe5
Marcos Serra <marcosserra133@gmail.com>
2023-09-06 00:06:14 -0300

    conversor de horas minutos para horas decimais

---
+++ horas-minutos/horas-para-minutos.py
@@ -0,0 +1,14 @@
+horas = input('Insira horas: ')
+minutos = input('Insira os minutos: ')
+
+horas = int(horas)
+minutos = float(minutos)
+
+minutos_decimal = minutos / 60
+minutos_formatados = "{:.2f}".format(minutos_decimal)
+
+# Verifique se os minutos são menores que 10 e, se sim, remova o zero à esquerda
+if minutos_decimal < 10:
+    minutos_formatados = minutos_formatados[1:]
+
+print(f'Sua hora em minutos decimais é {horas}{minutos_formatados}')
